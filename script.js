bbd993b3c72a8717e39eb167c4b15c08ef113d7a
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-02-15 16:56:55 +1300

    modified html, css and javascript files
    -Added elements and selectors to be used for styling and interactions in html
    -Added simple styling in css.
    -Added basic code in javascript for simple interactions using the dialog element.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Library")
+const openButton = document.querySelector("#open-btn");
+const closeButton = document.querySelector("#close-btn");
+const myDialog = document.querySelector("#myDialog")
+
+const author = document.querySelector("#author");
+const title = document.querySelector("#title");
+const pages = document.querySelector("#pages");
+const isRead = document.querySelector("#isRead");
+
+
+
+const displayBooks = document.querySelector("#display-ctn");
+
+
+openButton.addEventListener("click", () => {
+    myDialog.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    myDialog.close();
+})
