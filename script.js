50f6adf230c3871c35f111c53d1917516f756a81
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-02-17 18:44:59 +1300

    Added book-card display feature with remove button.
    -Added selector in html to be target by css and javascript
    -Style the book-card feature using css
    -Style the remove button feature using css
    -Added functions and listeners that will display the information of the book.
    -Added functions and listener that will remove the book-card by clicking the remove button.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const openButton = document.querySelector("#open-btn");
 const closeButton = document.querySelector("#close-btn");
 const myDialog = document.querySelector("#myDialog")
+const myForm = document.querySelector("#myForm")
+
 
 const author = document.querySelector("#author");
 const title = document.querySelector("#title");
@@ -19,3 +21,102 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     myDialog.close();
 })
+
+
+
+
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}. `; 
+
+}
+Book.pro
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", false);
+addBookToLibrary("The Mckarov", "MMM. Karov", "1998", true);
+
+
+
+
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+   displayTheBooks();
+
+   const titleValue = title.value;
+   const authorValue = author.value;
+   const pagesValue = pages.value;
+   const isReadValue = isRead.checked;
+
+
+   addBookToLibrary(titleValue, authorValue, pagesValue, isReadValue);
+
+   displayTheBooks();
+
+   myForm.reset();
+   
+   
+
+
+})
+
+
+function displayTheBooks() {
+    displayBooks.innerHTML = "";
+
+
+    myLibrary.forEach((book, index) => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookCard.setAttribute("data-index", index);
+
+        bookCard.innerHTML = `
+            <h3><strong>Title</strong>:${book.title}</h3>
+            <p><strong>Author</strong>:${book.author}</p>  
+            <p><strong>Pages</strong>:${book.pages}</p> 
+            <p><strong>Read</strong>:${book.read ? "Yes" : "No"}</p>
+
+            <button class="remove-btn" data-index="${index}">x</button>
+
+            
+        
+        `;
+
+        displayBooks.appendChild(bookCard)
+    });
+
+    removeButtonListener();
+}
+
+function removeIndex(index) {
+    myLibrary.splice(index, 1);
+    displayTheBooks();
+}
+
+function removeButtonListener() {
+    const removeButtons = document.querySelectorAll(".remove-btn");
+
+    removeButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const index = event.target.getAttribute("data-index");
+
+            removeIndex(index);
+        } )
+    })
+}
