cbafd3eb91a266e36dc12dd08bdbb56f3d749288
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-02-22 08:54:04 +1300

    Added toggle read button feature
    -Added functions to implement the toggle read button functionality
    -Styled the button.

--- script.js
+++ script.js
@@ -13,20 +13,10 @@ const isRead = document.querySelector("#isRead");
 
 const displayBooks = document.querySelector("#display-ctn");
 
-
-openButton.addEventListener("click", () => {
-    myDialog.showModal();
-})
-
-closeButton.addEventListener("click", () => {
-    myDialog.close();
-})
-
-
-
-
-
 const myLibrary = [];
+
+////////////////////////////////////////////////////////////////////////////////
+// Library functions
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -38,20 +28,97 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}. `; 
-
 }
-Book.pro
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", false);
-addBookToLibrary("The Mckarov", "MMM. Karov", "1998", true);
+
+function removeIndex(index) {
+    myLibrary.splice(index, 1);
+    displayTheBooks();
+}
 
 
 
+
+function removeButtonListener() {
+    const removeButtons = document.querySelectorAll(".remove-btn");
+
+    removeButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const index = event.currentTarget.getAttribute("data-index");
+
+            removeIndex(index);
+        } )
+    })
+}
+
+
+function toggleReadButton() {
+    let toggleButton = document.querySelectorAll(".toggle-read");
+
+    toggleButton.forEach(button => {
+        button.addEventListener("click", (event) => {
+          
+            const bookIndex = event.currentTarget.getAttribute("data-index");
+            myLibrary[bookIndex].toggleRead();
+            displayTheBooks();
+        } )
+    })
+}
+
+
+
+
+function displayTheBooks() {
+    displayBooks.innerHTML = "";
+
+
+    myLibrary.forEach((book, index) => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookCard.setAttribute("data-index", index);
+
+        bookCard.innerHTML = `
+            <h3><strong>Title</strong>:${book.title}</h3>
+            <p><strong>Author</strong>:${book.author}</p>  
+            <p><strong>Pages</strong>:${book.pages}</p> 
+            <p><strong><button class="toggle-read" data-index="${index}">Read</button></strong>:${book.read ? "Yes" : "No"}</p>
+
+            <button class="remove-btn" data-index="${index}">x</button>
+                      
+        
+        `;
+
+        displayBooks.appendChild(bookCard)
+    });
+
+    removeButtonListener();
+    toggleReadButton();
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////// 
+
+// Event Listeners
+
+
+openButton.addEventListener("click", () => {
+    myDialog.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    myDialog.close();
+})
 
 myForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -76,47 +143,7 @@ myForm.addEventListener('submit', function(event) {
 
 })
 
-
-function displayTheBooks() {
-    displayBooks.innerHTML = "";
-
-
-    myLibrary.forEach((book, index) => {
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("book-card");
-        bookCard.setAttribute("data-index", index);
-
-        bookCard.innerHTML = `
-            <h3><strong>Title</strong>:${book.title}</h3>
-            <p><strong>Author</strong>:${book.author}</p>  
-            <p><strong>Pages</strong>:${book.pages}</p> 
-            <p><strong>Read</strong>:${book.read ? "Yes" : "No"}</p>
-
-            <button class="remove-btn" data-index="${index}">x</button>
-
-            
-        
-        `;
-
-        displayBooks.appendChild(bookCard)
-    });
-
-    removeButtonListener();
-}
-
-function removeIndex(index) {
-    myLibrary.splice(index, 1);
-    displayTheBooks();
-}
-
-function removeButtonListener() {
-    const removeButtons = document.querySelectorAll(".remove-btn");
-
-    removeButtons.forEach(button => {
-        button.addEventListener("click", (event) => {
-            const index = event.target.getAttribute("data-index");
-
-            removeIndex(index);
-        } )
-    })
-}
+//////////////////////////////////////////////////////////////////////////////////
+// Initial Books
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", false);
+addBookToLibrary("The Mckarov", "MMM. Karov", "1998", true);
